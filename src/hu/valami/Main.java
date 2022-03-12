ae1788cd79fc4d9571e37a45060cd8c6dc498bfb
Patrik <csanyikpatrik@gmail.com>
2022-03-12 14:47:30 +0100

    gitignore

--- src/hu/valami/Main.java
+++ src/hu/valami/Main.java
@@ -1,8 +1,22 @@
 package hu.valami;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Kérek egy évszámot:");
+        System.out.print("> ");
+        int year = scanner.nextInt();
+
+        if((year>=1582) && (year%4 == 0))
+        {
+          System.out.println(year + " SZÖKŐÉV");
+        }
+        else if((year%100 == 0) && (year%400==0)) System.out.println("SZÖKŐÉV");
+        else System.out.println(year + "NEM SZÖKŐÉV");
+
     }
 }
