87e42d3ee5fab129fa634e2492681acdfccaf9f3
Patrik <csanyikpatrik@gmail.com>
2022-03-12 15:34:40 +0100

    gitignore

--- src/hu/valami/Main.java
+++ src/hu/valami/Main.java
@@ -19,7 +19,10 @@ public class Main {
         }
         else System.out.println(year + " NEM SZÖKŐÉV");*/
 
-        System.out.println("Kérek egy számot!");
+
+        // 2. feladat
+
+      /*  System.out.println("Kérek egy számot!");
         int szam = scanner.nextInt();
         int eredmeny;
         int counter = 0;
@@ -38,7 +41,35 @@ public class Main {
             }
             System.out.println("osztók száma: " + counter);
         }
+        else System.out.println("Nem természetes szám!");*/
+
+
+        // 3. feladat
+
+        System.out.println("Kérek egy számot!");
+        int szam = scanner.nextInt();
+        int eredmeny;
+        int counter = 0;
+
+        if(szam > 0)
+        {
+            for(int i = 1; i<=szam; i++)
+            {
+                eredmeny = szam/i;
+
+                if(szam%i == 0)
+                {
+                    counter++;
+                }
+            }
+            if(counter == 2)
+            {
+                System.out.println("Prímszám");
+            }
+            else System.out.println("Nem prímszám");
+        }
         else System.out.println("Nem természetes szám!");
+
 
 
 
