998749be22821223cf5f7736239e1dae3338e4fa
bertalan83 <e.bertalan96@gmail.com>
2022-03-16 19:53:34 +0100

    string to int number length

--- src/hu/valami/Gyakorlo3.java
+++ src/hu/valami/Gyakorlo3.java
@@ -1,4 +1,31 @@
 package hu.valami;
 
+import java.util.Scanner;
+
 public class Gyakorlo3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // 1. feladat
+
+        String number = scanner.nextLine();
+        int x;
+        x=number.length();
+        int hossz = Integer.parseInt(String.valueOf(number));
+
+
+        System.out.println(hossz);
+        System.out.println(x);
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
